3a7ce1c24b8cc359a2ba0bcfeec792b6baf586ee
tloth <toth.annar@gmail.com>
2019-04-05 12:44:25 +0100

    add updated boroughFunc back

--- src/scripts/boroughFunc.js
+++ src/scripts/boroughFunc.js
@@ -1,16 +1,37 @@
+//function to handle extremely nested api structure 
 const boroughFunction = json => {
+    
     const boroughObj = JSON.parse(json);
+    let arr = []
   
-    let arr = boroughObj['DailyAirQualityIndex']['LocalAuthority']['Site'].map(
-      x => {
-        return parseInt(x['Species'][0]['@AirQualityIndex']);
-      }
-    );
+    if ((boroughObj['DailyAirQualityIndex']['LocalAuthority']).hasOwnProperty('Site')){
+
+            if(Array.isArray(boroughObj['DailyAirQualityIndex']['LocalAuthority']['Site'])){
+                boroughObj['DailyAirQualityIndex']['LocalAuthority']['Site'].map(x => {
+                        if(Array.isArray(x['Species'])){
+                            x['Species'].map((y) => {
+                                arr.push(parseInt(y['@AirQualityIndex']));
+                            })}
+                            else {
+                                        arr.push(parseInt(x['Species']['@AirQualityIndex']));
+                                    }
+            })
+        } else {
+            if (Array.isArray(boroughObj['DailyAirQualityIndex']['LocalAuthority']['Site']['Species'])){
+                boroughObj['DailyAirQualityIndex']['LocalAuthority']['Site']['Species'].map( x => {
+                    arr.push(parseInt(x['@AirQualityIndex']));
+                })
+            }
+        }
   
-    let sum = arr.reduce((a, b) => a + b, 0);
-    let result = sum/arr.length;
-    return result;
-  };
   
-  module.exports = boroughFunction;
+        let sum = arr.reduce((a, b) => a + b, 0);
+        let result = sum/arr.length;
+    
+        return Math.round(result);
+    } else {
+        return 'Sorry, no data available'
+    }
+};
   
+module.exports = boroughFunction;
